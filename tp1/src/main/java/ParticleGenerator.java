a520883109f89eed8f4175613250efa7ced243ea
lucas <Lucas Gomez>
2021-03-14 23:12:22 -0300

    generador de particulas aleatorias

--- tp1/src/main/java/ParticleGenerator.java
+++ tp1/src/main/java/ParticleGenerator.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class ParticleGenerator {
-    private ArrayList<Particle> particles;
+    private ArrayList<ArrayList<Particle>> particles;
     private int i;
     private double maxRadius;
     private int l;
@@ -12,26 +12,31 @@ public class ParticleGenerator {
     }
 
     //genera 'n' particulas variando su posicion entre 0 y 'l'; con radio entre 0 y 'maxRadius'
-    public void generate(int n, int l, double maxRadius){
+    public ArrayList<ArrayList<Particle>> generate(int n, int l, double maxRadius){
         this.l = l;
         for (i = 0; i < n; i++){
             Particle aux = createParticle(maxRadius);
             if (this.particles.isEmpty()){
-                this.particles.add(aux);
+                this.particles.add(new ArrayList<>());
+                this.particles.get(0).add(aux);
             } else {
-                for (Particle p : this.particles) {
-                    aux = checkPosition(aux, p);
+                for (ArrayList<Particle> parts : this.particles) {
+                    for (Particle p : parts){
+                        aux = checkPosition(aux, p);
+                    }
                 }
+                this.particles.get(0).add(aux);
             }
         }
+        return this.particles;
     }
 
     //Evita superposicion de particulas
     private Particle checkPosition(Particle p, Particle par){
-        for (int count = 0; count < 100 && p.getX() <= par.getX() - par.getRadius() && p.getX() >= par.getX() + par.getRadius(); count++){
+        for (int count = 0; count < 100 && p.getX() <= par.getX() - p.getRadius() - par.getRadius() && p.getX() >= par.getX() + p.getRadius() + par.getRadius(); count++){
             p = createParticle(this.maxRadius);
         }
-        for (int count = 0; count < 100 && p.getY() <= par.getY() - par.getRadius() && p.getY() >= par.getY() + par.getRadius(); count++){
+        for (int count = 0; count < 100 && p.getY() <= par.getY() - p.getRadius() - par.getRadius() && p.getY() >= par.getY() + p.getRadius() + par.getRadius(); count++){
             p = createParticle(this.maxRadius);
         }
         return p;
