a24c041fbbf8d3ded9934e2e57f0557197493fe1
Guido Princ <guido.princ@wolox.com.ar>
2021-03-15 00:57:44 -0300

    added better manage on mai

--- tp1/src/main/java/Main.java
+++ tp1/src/main/java/Main.java
@@ -11,30 +11,41 @@ public class Main {
         ArrayList<ArrayList<Double>> staticData = fm.readNumericFile("Static100.txt");
         ArrayList<ArrayList<Double>> dynamicData = fm.readNumericFile("Dynamic100.txt");
 
-        int N = staticData.get(0).get(0).intValue();
-        int L = staticData.get(1).get(0).intValue();
-        double r_c = 5;
+        int L = staticData.get(0).get(0).intValue();
+        int N = staticData.get(1).get(0).intValue();
+        double r_c = staticData.get(2).get(0).doubleValue();
+        int M = staticData.get(3).get(0).intValue();
+        double r = 0.25;
+        boolean hasR = false;
+        if (staticData.size() > 4) {
+            hasR = true;
+            r = staticData.get(4).get(0).doubleValue();
+        }
 
-        ParticleGenerator pg = new ParticleGenerator(0.25);
+        ParticleGenerator pg = new ParticleGenerator(r, hasR);
 
         ArrayList<Particle> particles = new ArrayList<>();
-        ArrayList<ArrayList<Particle>> generatedParticles = pg.generate(N,L,0.25);
-        if (staticData.size() < 5) {
-            fm.createResults(generatedParticles, "generatedParticles.txt");
+        ArrayList<ArrayList<Particle>> generatedParticles = pg.generate(N,L);
+        if (staticData.size() < 6) {
+            fm.createResults(generatedParticles, "Dynamic.txt");
             for (ArrayList<Particle> arr_p: generatedParticles)
                 for (Particle p: arr_p)
                     particles.add(p);
         } else {
-            for (int i = 1; i < 1 + N; i++)
+            for (int i = 6; i < 6 + N; i++)
                 particles.add(new Particle(dynamicData.get(i).get(1), dynamicData.get(i).get(0), staticData.get(i+1).get(0), i - 1));
         }
 
         long startTime = System.currentTimeMillis();
-        CellIndex cellIndex = new CellIndex(N, L,5, r_c, true, particles);
+        CellIndex cellIndex = new CellIndex(N, L,M, r_c, true, particles);
         cellIndex.setNeighbour();
         long endTime = System.currentTimeMillis();
-        System.out.println("That took " + (endTime - startTime) + " milliseconds");
-        fm.createResults(cellIndex.getOutput(),  System.nanoTime(), "particles.txt");
-        fm.createResults(cellIndex.getOutput(), 0, "particles.txt");
+        System.out.println("Periodic took " + (endTime - startTime) + " milliseconds");
+        fm.createResults(cellIndex.getOutput(),  0, "particlesPeriodic.txt");
+
+        cellIndex = new CellIndex(N, L,M, r_c, false, particles);
+        cellIndex.setNeighbour();
+        System.out.println("Not periodic took " + (System.currentTimeMillis() - endTime) + " milliseconds");
+        fm.createResults(cellIndex.getOutput(),  0, "particlesNotPeriodic.txt");
     }
 }
--- tp1/src/main/java/ParticleGenerator.java
+++ tp1/src/main/java/ParticleGenerator.java
@@ -4,21 +4,23 @@ import java.util.ArrayList;
 
 public class ParticleGenerator {
     private double maxRadius;
+    private boolean notRandom;
     private int l;
 
-    public ParticleGenerator(double maxRadius){
+    public ParticleGenerator(double maxRadius, boolean notRandom){
         this.maxRadius = maxRadius;
+        this.notRandom = notRandom;
     }
 
     //genera 'n' particulas variando su posicion entre 0 y 'l'; con radio entre 0 y 'maxRadius'
-    public ArrayList<ArrayList<Particle>> generate(int n, int l, double maxRadius){
+    public ArrayList<ArrayList<Particle>> generate(int n, int l){
         ArrayList<ArrayList<Particle>> particles = new ArrayList<>();
         ArrayList<Particle> auxArray;
         ArrayList<Particle> allGenerated = new ArrayList<>();
         this.l = l;
         for (int i = 0; i < n; i++){
             auxArray = new ArrayList<>();
-            Particle aux = createParticle(maxRadius, i);
+            Particle aux = createParticle(i);
             if (allGenerated.isEmpty()){
                 auxArray.add(aux);
             } else {
@@ -35,19 +37,21 @@ public class ParticleGenerator {
     //Evita superposicion de particulas
     private Particle checkPosition(Particle p, Particle par, int i){
         for (int count = 0; count < 100 && p.getX() <= par.getX() - p.getRadius() - par.getRadius() && p.getX() >= par.getX() + p.getRadius() + par.getRadius(); count++){
-            p = createParticle(this.maxRadius, i);
+            p = createParticle(i);
         }
         for (int count = 0; count < 100 && p.getY() <= par.getY() - p.getRadius() - par.getRadius() && p.getY() >= par.getY() + p.getRadius() + par.getRadius(); count++){
-            p = createParticle(this.maxRadius, i);
+            p = createParticle(i);
         }
         return p;
     }
 
 
-    private Particle createParticle(double maxRadius, int i) {
+    private Particle createParticle(int i) {
         double x = Math.random() * l;
         double y = Math.random() * l;
-        double radius = Math.random() * maxRadius;
+        double radius = Math.random() * this.maxRadius;
+        if (notRandom)
+            radius = this.maxRadius;
         return new Particle(x, y, radius, i);
     }
 }
