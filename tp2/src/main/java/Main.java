0165a0bc61c3236ca6ffb73d2f7fa2cbdb712c26
santiagoburgos <sburgos@itba.edu.ar>
2021-04-03 19:46:09 -0300

    stats to csv

--- tp2/src/main/java/Main.java
+++ tp2/src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,22 +19,45 @@ public class Main {
 
 
         LifeCells lc = new LifeCells( 100, cells, Rule.FREDKIN, true);
+
+        try {
+            createFile("2dstats.cvs", lc.maxDistance, lc.aliveCells, lc.finalTime);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         for (int i = 0; i <= lc.finalTime; i++) {
             OvitoGen.saveDynamicFile(i,lc.aliveCells.get(i), lc.lifeCells,"D:\\OV\\");
-        //    System.out.println("time " + i + " maxdist " + lc.maxDistance.get(i) + " aliveCells " + lc.aliveCells.get(i) );
-
         }
 
 
 
         LifeCells lctd = new LifeCells( 100, cellsTD, Rule.FREDKIN, true);
+
+        try {
+            createFile("3dstats.cvs", lctd.maxDistanceTD, lctd.aliveCellsTD, lctd.finalTime);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         for (int i = 0; i <= lctd.finalTime; i++) {
            OvitoGen.saveDynamicFileTD(i, lctd.aliveCellsTD.get(i), lctd.lifeCellsTD,"D:\\OVTD\\");
-          // System.out.println("time " + i + " maxdist " + lctd.maxDistanceTD.get(i) + " aliveCells " + lctd.aliveCellsTD.get(i) );
-
         }
 
 
+    }
+
+    public static void createFile(String fileName, Map<Integer, Integer> maxDistance, Map<Integer, Integer> aliveCells, int time)
+            throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+
+        writer.write("time,maxdistance,alivecells\n");
+        for (int i = 0; i <= time; i++) {
+            writer.write(i+","+maxDistance.get(i)+","+aliveCells.get(i)+"\n");
+        }
+
+
+        writer.close();
     }
 
 
