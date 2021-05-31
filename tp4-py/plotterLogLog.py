19d7ea88f8e32fe2f6d1230aeedce0361e21f464
Guido Princ <guido.princ@wolox.com.ar>
2021-05-30 22:24:33 -0300

    corresponding loglog

--- tp4-py/plotterLogLog.py
+++ tp4-py/plotterLogLog.py
@@ -1,10 +1,40 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
-# Data for plotting
-t = np.arange(0.01, 20.0, 0.01)
+# Create corresponding txt files
+t = [0.001, 0.0001, 0.00001, 0.000001]
 
-plt.loglog(t, 20 * np.exp(-t / 10.0))
+filepath = "verletECMs.txt"
+file = open(filepath)
+verletECMs = []
+lines = file.readlines()
+for line in lines:
+   verletECMs.append(float(line))
+file.close()
+
+filepath = "beemanECMs.txt"
+file = open(filepath)
+beemanECMs = []
+lines = file.readlines()
+for line in lines:
+   beemanECMs.append(float(line))
+file.close()
+
+filepath = "gpcoECMs.txt"
+file = open(filepath)
+gpcoECMs = []
+lines = file.readlines()
+for line in lines:
+   gpcoECMs.append(float(line))
+file.close()
+
+
+plt.loglog(t, verletECMs, 'ro', label='Verlet')
+plt.loglog(t, beemanECMs, 'go', label='Beeman')
+plt.loglog(t, gpcoECMs, 'bo', label='GPCO5')
 plt.grid()
+plt.xlabel('Paso del Tiempo (segundos)')
+plt.ylabel('Error Cuatrático Medio (m^2)')
+plt.legend()
 
 plt.show()
+
