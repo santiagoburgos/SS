dd6a20f577a28a16d4516b11897b607b82f92869
Guido Princ <guido.princ@wolox.com.ar>
2021-06-06 14:03:14 -0300

    size and fix data

--- tp5-py/ejercicio-b.py
+++ tp5-py/ejercicio-b.py
@@ -25,24 +25,24 @@ time = []
 
 for line in lines:
     data = line.split(',')
-    people.append(float(data[0]))
-    time.append(float(data[1]))
+    people.append(float(data[1]))
+    time.append(float(data[0]))
 
 for line in lines2:
     data = line.split(',')
-    people2.append(float(data[0]))
+    people2.append(float(data[1]))
 
 for line in lines3:
     data = line.split(',')
-    people3.append(float(data[0]))
+    people3.append(float(data[1]))
 
 for line in lines4:
     data = line.split(',')
-    people4.append(float(data[0]))
+    people4.append(float(data[1]))
 
 for line in lines5:
     data = line.split(',')
-    people5.append(float(data[0]))
+    people5.append(float(data[1]))
 
 mean = []
 desv = []
@@ -51,6 +51,7 @@ for x in range(0, len(people)):
    aux = pow(people[x], 2) + pow(people2[x], 2) + pow(people3[x], 2) + pow(people4[x], 2) + pow(people5[x], 2)
    desv.append(math.sqrt((aux - (5 * pow(mean[x], 2))) / 5))
 
+plt.rcParams.update({'font.size': 14})
 plt.errorbar(time, mean, xerr=desv)
 plt.xlabel('Tiempo (segundos)')
 plt.ylabel('Egresos')
